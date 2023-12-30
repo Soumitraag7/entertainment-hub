f9ceb2318da76df560ab3484cdd522d6277c69bd
Soumitraag7 <71151027+Soumitraag7@users.noreply.github.com>
2023-12-30 18:42:02 +0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react';
+
+import Header from './components/Header/Header';
 import './App.css';
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<>
-			<span>Header</span>
-		</>
+		<div className="app">
+			<Header />
+		</div>
 	);
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return <span className="header">🎬 Entertainment Hub 🎥</span>;
+}
+
+export default Header;
